488339a3491e003017af27981129ee5299445781
tnylea <tnylea@gmail.com>
2014-05-31 19:38:53 -0700

    Adding the beginning of test script and a few other files

--- tests/acceptance/06-UploadTestsCept.php
+++ tests/acceptance/06-UploadTestsCept.php
@@ -11,12 +11,22 @@ $I->fillField('#password_confirmation','password');
 $I->click('.form-signin button');
 $I->see('successful');
 
+
+// Upload picture from file
 $I->click('.upload-btn-desktop');
 $I->seeElement('.icon-cloud-upload');
-
 $I->fillField('#title','Bobs Burgers');
 $I->attachFile('#pic_url', 'bobs-burgers.jpg');
 $I->click('.submit-media');
+$I->see('Bobs Burgers');
+
+// Upload picture from URL
+$I->click('.upload-btn-desktop');
+$I->seeElement('.icon-cloud-upload');
+$I->fillField('#title','Cool Burgers Bob');
+$I->fillField('#img_url', 'https://raw.githubusercontent.com/tnylea/ninjamediascript-bddtests/master/tests/_data/bobs-burgers2.jpg');
+$I->click('.submit-media');
+$I->see('Cool Burgers Bob');
 
 
 
